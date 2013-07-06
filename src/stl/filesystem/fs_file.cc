16d3c332973c3e55aa41f386c8e1d6293166ce83
Tim Peters <mail@darksecond.nl>
2013-07-06 20:50:02 +0200

    added write support

--- src/stl/filesystem/fs_file.cc
+++ src/stl/filesystem/fs_file.cc
@@ -4,7 +4,6 @@
 
 stl::fs_file::fs_file(const char* path, int mode, archive* owner) : stl::file(owner)
 {
-    //TODO: cannot set an ifstream to out mode!
     std::ios_base::openmode f_mode = std::ios_base::binary;
     if(mode & fs_modes::in)
         f_mode = f_mode | std::ios_base::in;
@@ -13,19 +12,28 @@ stl::fs_file::fs_file(const char* path, int mode, archive* owner) : stl::file(ow
     if(mode & fs_modes::append)
         f_mode = f_mode | std::ios_base::app;
     
-    _stream = std::ifstream(path, f_mode);
+    _stream = std::fstream(path, f_mode);
 }
 
 size_t stl::fs_file::read(void* buffer, size_t buf_size)
 {
-    char* char_buffer = reinterpret_cast<char*>(buffer);
-    _stream.read(char_buffer, buf_size);
+    _stream.read(static_cast<char*>(buffer), buf_size);
     return _stream.gcount();
+}
+
+void stl::fs_file::write(const void* buffer, size_t buf_size)
+{
+    _stream.write(static_cast<const char*>(buffer), buf_size);
 }
 
 void stl::fs_file::skip(size_t amount)
 {
     _stream.seekg(amount, std::ios_base::cur);
+}
+
+void stl::fs_file::seek(size_t position)
+{
+    _stream.seekg(position, std::ios_base::beg);
 }
 
 bool stl::fs_file::eof() const
--- src/stl/filesystem/fs_file.h
+++ src/stl/filesystem/fs_file.h
@@ -8,12 +8,14 @@ namespace stl
 {
     class fs_file : public file
     {
-        std::ifstream _stream;
+        std::fstream _stream;
     public:
         fs_file(const char* path, int mode, archive* owner);
         
         virtual size_t read(void* buffer, size_t buf_size);
+        virtual void write(const void* buffer, size_t buf_size);
         virtual void skip(size_t amount);
+        virtual void seek(size_t position);
         virtual bool eof() const;
         virtual size_t total_size();
     };
